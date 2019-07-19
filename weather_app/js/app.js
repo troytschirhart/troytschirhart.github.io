10a550c0cffc3989a3c77013bd4c63a3608187f8
Troy Tschirhart <troytschirhart@Troys-MacBook-Pro.local>
2019-07-19 15:09:25 -0600

    Able to parse date, time and year, adjust for local time zone, and correct for ripple effects in time, month and year (including leap years)

--- weather_app/js/app.js
+++ weather_app/js/app.js
@@ -1,22 +1,57 @@
 
 
+
+//==================================
+//  GLOBAL VARIABLES
+//==================================
+
+let urlString = "";
+
+
+
+
 //===============================  Start of on load  =======================
 $(() => {
 
+//==================================
+//  EVENT LISTENERS
+//==================================
+
+$(() => {
+  $('form').on('submit', (event) => {
+
+    const zipCode = $('#input-box').val();
 
 
-  $.ajax({
-    url: "http://api.openweathermap.org/data/2.5/forecast?zip=80922,us&APPID=5d118fc36b1c769a2923778a4df26bd0",
-  }).then(
-    (data) => {
-      console.log("forecast date and UTC time: " + data.list[0].dt_txt);
-      console.log("temperature in degrees Kelvin: " + data.list[0].main.temp);
-      console.log("percent humidity: " + data.list[0].main.humidity);
-      console.log("wind speed in meters per second: " + data.list[0].wind.speed);
-      console.log("direction from which wind originates in degrees: " + data.list[0].wind.deg);
-      console.log("overall weather conditions: " + data.list[0].weather[0].description);
-    }
-  )
+    $.ajax({
+      url: "http://api.openweathermap.org/data/2.5/forecast?zip=" + zipCode + ",us&APPID=5d118fc36b1c769a2923778a4df26bd0"
+    }).then(
+      (data) => {
+        console.log("forecast date and UTC time: " + data.list[0].dt_txt);
+        console.log("temperature in degrees Kelvin: " + data.list[0].main.temp);
+        console.log("percent humidity: " + data.list[0].main.humidity);
+        console.log("wind speed in meters per second: " + data.list[0].wind.speed);
+        console.log("direction from which wind originates in degrees: " + data.list[0].wind.deg);
+        console.log("overall weather conditions: " + data.list[0].weather[0].description);
+      }
+    )
+
+
+
+    // reset the form and prevent default actions
+        $(event.currentTarget).trigger('reset')
+        return false;
+
+  })
+})
+
+
+
+
+
+
+
+
 
 
 
